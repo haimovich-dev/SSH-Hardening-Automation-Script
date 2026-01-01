8fc48e5737fc1ba9839f6a9d38bd0450e2094d56
haimovich-dev <128306995+haimovich-dev@users.noreply.github.com>
2026-01-01 19:46:20 +0000

    cleanup function added and openssl_build function changed and config.conf file added to provide static data during installation

--- harden-ssh.sh
+++ harden-ssh.sh
@@ -6,11 +6,11 @@ ssh_packages=($(apt list --installed > /dev/null 2>&1 | grep ssh | cut -d/ -f1))
  
 ssh_keys_bkp="/tmp/ssh-keys-bkp"
 
+source "config.conf"
+
 # OpenSSL related variables
 
-openssl_tarrball='https://github.com/openssl/openssl/releases/download/openssl-3.5.1/openssl-3.5.1.tar.gz'
-openssl_checksum='529043b15cffa5f36077a4d0af83f3de399807181d607441d734196d889b641f'
-openssl_version="$(echo "$openssl_tarrball" | grep -oP 'openssl-\K[0-9]+\.[0-9]+\.[0-9]+' | head -n1)"
+openssl_version="$(echo "$OPENSSL_TARBALL_URL" | grep -oP 'openssl-\K[0-9]+\.[0-9]+\.[0-9]+' | head -n1)"
 openssl_tarball_path="/opt/openssl-${openssl_version}.tar.gz"
 openssl_path="/opt/openssl-${openssl_version}"
 
@@ -23,6 +23,18 @@ openssh_tarball_path="/opt/openssh-${openssh_version}.tar.gz"
 openssh_path="/opt/openssh-${openssh_version}"
 
 LOG_FILE="/home/danil/scripts/SSH-Hardening-Automation-Script/log"
+
+# description: cleans up all the remained files from the system
+# return: nothing
+function cleanup(){
+    #openssl
+    sudo rm -r $openssl_tarball_path &>/dev/null
+    write_log 1 "$openssl_tarball_path successfully removed"
+    sudo rm -r '/opt/openssl' &>/dev/null
+    write_log 1 "/opt/openssl successfully removed"
+    sudo rm -r $openssl_path &>/dev/null
+    write_log 1 "$openssl_path successfully removed"
+}
 
 # description: saves logs
 # arguments:
@@ -50,9 +62,10 @@ function write_log(){
     echo -e "${prefix} ${desc}"
 }
 
-# description: Checks for privilleges
-# return: exit code (0 for success, non-zero for failure)
+# description: checks for privilleges
+# return: exit code (nothing for success, non-zero for failure)
 function privileges(){
+    write_log 3 "Checking privileges"
     current_user="$(whoami)"
     if [ "$current_user" != "root" ]; then
         write_log 2 "Current user is not root"
@@ -81,8 +94,9 @@ function privileges(){
 }
 
 # description: scans for required packages
-# return: exit code (0 for success, non-zero for failure)
+# return: exit code (nothing for success, non-zero for failure)
 function packages(){
+    write_log 3 "Installing packages"
     for pkg in "${required_packages[@]}"; do
         if command -v "$pkg" &>/dev/null; then
             write_log 1 "${pkg} is Installed"
@@ -100,37 +114,47 @@ function packages(){
 }
 
 # description: fetching, extraction, configuring and building OpenSSL 
-# return: exit code (0 for success, non-zero for failure)
+# return: exit code (nothing for success, non-zero for failure)
 function openssl_build(){
     write_log 3 "Downloading OpenSSL ${openssl_version}"
-    if ! sudo wget -P '/opt' $openssl_tarrball &>/dev/null; then
+    if ! sudo wget -P '/opt' $OPENSSL_TARBALL_URL &>/dev/null; then
         write_log 2 "Download Failed"
         exit 1
     else
-        if [ "$(openssl dgst -sha256 "$openssl_tarball_path" | awk '{print $2}')" = "$openssl_checksum" ]; then
+        if [ "$(sha256sum "$openssl_tarball_path" | awk '{print $1}')" = "$OPENSSL_CHECKSUM" ]; then
             write_log 1 "Checksums Match"
         else
             write_log 2 "Checksums don't match! (possible mitigation attack)"
             exit 1
         fi
-        write_log 1 "OpenSSL ${openssl_version} Downloaded successfully"
+        write_log 1 "OpenSSL ${openssl_version} Downloaded and verified successfully"
+    fi
+
+    write_log 3 "Creating extraction path"
+    if ! sudo mkdir $openssl_path  &>/dev/null; then
+        write_log 2 "Failed to create ${$openssl_path}"
+        exit 1
+    else
+        write_log 1 "${$openssl_path} created"
     fi
 
     write_log 3 "Extracting OpenSSL files"
-    #
-    sudo mkdir $openssl_path  &>/dev/null
-    #
     if ! sudo tar -xvf $openssl_tarball_path -C $openssl_path --strip-components=1 &>/dev/null; then
         write_log 2 "Extraction Failed"
         exit 1
     else
         write_log 1 "Extraction succeded"
-        sudo rm -r $openssl_tarball_path  &>/dev/null
-        write_log 1 "OpenSSL tarball successfully removed"
+    fi
+
+    write_log 3 "Creating /opt/openssl"
+    if ! sudo mkdir /opt/openssl &>/dev/null; then
+        write_log 2 "Failed to create /opt/openssl"
+        exit 1
+    else
+        write_log 1 "/opt/openssl created"
     fi
 
     write_log 3 "Building OpenSSL ${openssl_version}"
-    sudo mkdir /opt/openssl &>/dev/null
     if ! (cd $openssl_path && sudo "./Configure" -fPIC --prefix="/opt/openssl" --openssldir="/etc/ssl/openssl-${openssl_version}" no-shared &>/dev/null); then
         write_log 2 "Configuration Failed"
         exit 1
@@ -152,6 +176,6 @@ function openssl_build(){
     fi
 }
 
-privileges
-packages
-openssl_build
+#privileges
+#packages
+#openssl_build
